6836a34c102ad2b7cd9f059bef88c6165a8d78fa
akod0883 <akhilkodumuri9@gmail.com>
2016-12-27 20:35:30 -0600

    added a sample

---
+++ Practice.py
@@ -0,0 +1,18 @@
+x = (input("Enter a palindrome ", ))
+
+def reverse(string):
+    begin = 0
+    end = len(string) - 1
+    strlist = [i for i in string]
+    while(begin < end):
+        temp = strlist[begin]
+        strlist[begin] = strlist[end]
+        strlist[end] = temp
+        begin += 1
+        end -= 1
+    return ''.join(strlist)
+
+if reverse(x.lower()) == x.lower():
+    print("This is is a palindrome")
+else:
+    print("Not a palindrome")
