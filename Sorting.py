a4ecd77bb62547bd3244574e2e279a0db8a59e03
akod0883 <akhilkodumuri9@gmail.com>
2016-12-28 18:55:09 -0600

    new commit

--- Sorting.py
+++ Sorting.py
@@ -1,1 +1,5 @@
-print ("Enter a list of numbers")
+numbers = input("Enter list of numbers seperated by spaces ").split(" ")
+print(sorted(numbers, key=int))
+import time
+start_time = time.time()
+print("--- %s seconds ---" % (time.time() - start_time))
